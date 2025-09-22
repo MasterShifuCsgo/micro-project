480571c472b6a78eef2205b7cdd8f79568897023
Kaspar Bergert <50704267035@tptlive.ee>
2025-09-22 10:52:52 +0300

    feat: created server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,4 +1,17 @@
+import express from "express";
 
+const app = express();
+
+const PORT = process.env.PORT;
+
+
+
+
+app.listen(PORT, () =>  {
+  console.log("server running on port: 3000");
+}).on("error", (error) => {
+  consle.log("Failed to start server", error);
+})
 
 
 
