e085420c861d8124cb119e6f57a78a1f5003cb49
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-10-29 15:13:15 +0200

    Feat: dashboard dynamic view

--- Frontend/src/Pages/Dashboard.vue
+++ Frontend/src/Pages/Dashboard.vue
@@ -1,4 +1,9 @@
 <script setup>
+const hinnangud = [
+  { nimetus: 'Keeled ja humanitaarteadused', hinne: '4/5' },
+  { nimetus: 'Matemaatika ja loodusteadused', hinne: '4/5' },
+  { nimetus: 'Praktilised ja tehnoloogia ained', hinne: '4/5' },
+];
 
 </script>
 <template>
@@ -7,30 +12,69 @@
     <div class="areas">
       <h1>Kommentaaride statistika</h1>
       <main>
-        <div class="oppekavade_statistika">
-        <div>
-            <h1 >Õppekavad</h1>
-            <div>Keeled ja humanitaarteadused</div>
-            <div>Matemaatika ja loodusteadused</div>
-            <div>Praktilised- ja tehnoloogia ained</div>
-            <div>Kunsti- ja liikumisained</div>
-            <div>Sotsiaalained</div>
+        <div class="oppekavade-statistika py-4">
+          <div class="oppekavade-statistika py-4">
+            <div class="container">
+
+              <div class="row border-bottom mb-2 pb-1 fw-bold">
+                <div class="col-sm-8 text-start">
+                  Nimetus
+                </div>
+                <div class="col-sm-4 text-start">
+                  Hinnang
+                </div>
+              </div>
+
+              <!-- Rea kordused -->
+              <div class="row align-items-center py-2 border-bottom" v-for="(item, i) in hinnangud" :key="i">
+                <div class="col-sm-8 text-start">
+                  {{ item.nimetus }}
+                </div>
+                <div class="col-sm-4 text-start fw-semibold">
+                  {{ item.hinne }}
+                </div>
+              </div>
+            </div>
           </div>
+
         </div>
         <div class="kommentaaride_statistika">
-        2</div>
+        </div>
       </main>
     </div>
   </div>
 
 </template>
 <style lang="css">
+.oppekavade-statistika {
+  background-color: #f8f9fa;
+  border-radius: 10px;
+  max-width: 700px;
+  margin: 0 auto;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.05);
+  height: 50%; 
+}
+
+.oppekavade-statistika .row {
+  font-size: 1rem;
+}
+
+.oppekavade-statistika .row:last-child {
+  border-bottom: none;
+}
+
+.oppekavade-statistika .col-sm-8,
+.oppekavade-statistika .col-sm-4 {
+  padding-left: 1rem;
+}
+
+
 .container {
   display: flex;
   flex-direction: column;
-  height: 100svh;  
-  justify-items: center;  
-  align-items: center;  
+  height: 100svh;
+  justify-items: center;
+  align-items: center;
 }
 
 main {
